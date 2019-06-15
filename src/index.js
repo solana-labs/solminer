0015932bde37499303c51b8c8a2f94686267f4d7
Michael Vines <mvines@gmail.com>
2019-06-14 20:15:24 -0700

    fix: shrink window

--- src/index.js
+++ src/index.js
@@ -20,8 +20,8 @@ if (isDevMode) enableLiveReload({strategy: 'react-hmr'});
 const createWindow = async () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 200,
   });
 
   // and load the index.html of the app.
