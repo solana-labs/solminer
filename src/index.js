f073b050cf874174c5a9bc3e493af6f89c325f60
Michael Vines <mvines@gmail.com>
2019-06-20 19:32:33 -0700

    refactor: clean up index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import installExtension, {
 } from 'electron-devtools-installer';
 import {enableLiveReload} from 'electron-compile';
 import updateElectronApp from 'update-electron-app';
+import path from 'path';
 
+// Adjust app name when in dev mode (don't want 'Electron' as the app name)
+app.setName('solminer');
+app.setPath('userData', path.join(app.getPath('appData'), app.getName()));
 
 try {
   updateElectronApp();
@@ -12,20 +16,17 @@ try {
   console.error(`Unable to enable updates: ${err}`);
 }
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-
 const isDevMode = process.execPath.match(/[\\/]electron/);
-
 if (isDevMode) enableLiveReload({strategy: 'react-hmr'});
 
-const createWindow = async () => {
+let mainWindow;
+
+app.on('ready', async () => {
   const devModeExtra = isDevMode ? 500 : 0;
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 500 + devModeExtra,
-    height: 200 + devModeExtra,
+    height: 500 + devModeExtra,
   });
 
   // and load the index.html of the app.
@@ -44,29 +45,8 @@ const createWindow = async () => {
     // when you should delete the corresponding element.
     mainWindow = null;
   });
-};
+});
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
