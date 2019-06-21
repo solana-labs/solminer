f5fa5ba9b1f8537a69764c784880b48f4a279aaf
Michael Vines <mvines@gmail.com>
2019-06-20 20:19:44 -0700

    refactor: add solana-install-init.js

---
+++ src/solana-install-init.js
@@ -0,0 +1,10 @@
+// Resolves the location of the solana-install-init program
+import os from 'os';
+import path from 'path';
+
+const isDevMode = process.execPath.match(/[\\/]electron/);
+const dotexe = os.type() === 'Windows_NT' ? '.exe' : '';
+
+export const solanaInstallInit = isDevMode
+  ? `solana-install-init${dotexe}` // in dev mode assume it's in the path
+  : path.join(process.resourcesPath, `solana-install-init${dotexe}`); // bundled in the app when not in dev mode
