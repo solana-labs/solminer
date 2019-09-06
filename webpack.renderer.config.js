b93d7f4d11391258bc1ebfe022154c49208811de
Manuel Calavera <diserkin@gmail.com>
2019-09-06 14:54:11 -0700

    update forge-electron and rebase project on webpack
    electon compile is depricated

---
+++ webpack.renderer.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const rules = require('./webpack.rules');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+      styles: path.resolve(__dirname, './src/styles'),
+      components: path.resolve(__dirname, './src/components'),
+      store: path.resolve(__dirname, './src/store'),
+    },
+  },
+  module: {
+    rules,
+  },
+};
