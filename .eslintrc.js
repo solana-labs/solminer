837f5c9f43a62e4e2547b723d77080b3bf6ddf75
Manuel Calavera <diserkin@gmail.com>
2019-09-06 14:54:11 -0700

    update forge-electron and rebase project on webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,28 @@
 module.exports = {
-  extends: 'eslint-config-airbnb',
   parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 8,
   },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['import', 'prettier', 'react'],
   rules: {
-    'arrow-parens': ['error', 'as-needed'],
-    'import/extensions': 0,
-    'import/no-extraneous-dependencies': 0,
-    'import/no-unresolved': [2, {ignore: ['electron']}],
-    'import/prefer-default-export': 0,
-    'linebreak-style': ['error', 'unix'],
-    'no-console': 0,
-    'object-curly-spacing': ['error', 'never'],
-    'react/prefer-stateless-function': 0,
-    'react/sort-comp': 0,
+    'prettier/prettier': 'error',
+    'react/jsx-props-no-spreading': 'off',
+    'react/require-default-props': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['components', './src/components'],
+          ['store', './src/store'],
+        ],
+        extensions: ['.js', '.jsx', '.json']
+      },
+    }
   },
 };
