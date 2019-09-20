1424c745d5ebc80214171c5bb2ee231f865ea9f8
Manuel Calavera <diserkin@gmail.com>
2019-09-19 20:38:00 -0700

    fix: wallet settings related bug fixes

--- src/components/ChangeWallet/index.jsx
+++ src/components/ChangeWallet/index.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppStore } from 'store';
 import { Card, Button, Typography, Input } from 'components/UI';
+import { useTranslation } from 'react-i18next';
 import CreateWalletBtn from 'components/CreateWalletBtn';
 import CloseIcon from '../../images/icon/close.svg';
 import css from './styles.module.scss';
+import generateKeys from '../../utils/generateKeys';
 
 const Change = () => {
-  const { setScreen } = AppStore;
-  const handleSubmit = () => setScreen('mining');
+  const { t } = useTranslation();
+  const {
+    setScreen,
+    setSecretKey,
+    setDepositPublicKey,
+    depositPublicKey,
+  } = AppStore;
+  const [seedPhrase, setSeedPhrase] = useState('');
+
+  const handleSeedChange = e => setSeedPhrase(e.target.value);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { publicKey, secretKey } = generateKeys(seedPhrase);
+    setSecretKey(secretKey);
+    setDepositPublicKey(publicKey);
+  };
+
   const close = () => setScreen('mining');
   return (
     <div>
@@ -18,19 +36,25 @@ const Change = () => {
         </button>
       </Typography>
       <div className={css.step}>
-        <Typography type="subttl">Current wallet</Typography>
-        <Typography>0xAA15A3E6b97d09653b8b8d9c9e1D80daf5ba81e8</Typography>
+        <Typography type="subttl">{t('current_wallet')}</Typography>
+        <Typography>{depositPublicKey}</Typography>
       </div>
       <Card>
         <form noValidate onSubmit={handleSubmit}>
           <Typography className={css.formTitle} type="cardTitle">
-            enter a valid seed phrase to connect your wallet
+            {t('enter_seed_valid_phrase')}
           </Typography>
-          <Input placeholder="Enter seed phrase" />
+          <Input
+            value={seedPhrase}
+            onChange={handleSeedChange}
+            placeholder={t('enter_seed_phrase')}
+          />
           <Typography className={css.formHint}>
-            Seed phrase should be 12 words in length.
+            {t('seed_phrase_reqs')}
           </Typography>
-          <Button type="submit">Change wallet</Button>
+          <Button type="submit" disabled={!seedPhrase}>
+            {t('connect_wallet')}
+          </Button>
         </form>
       </Card>
       <CreateWalletBtn horizontal />
