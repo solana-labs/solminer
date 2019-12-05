246452511db37613df16ac89c3ab0f43ddce305c
Evgeny Shvingelsky <evgeny.shvingelsky@zensoft.io>
2019-12-05 11:52:29 +0300

    added validation

--- src/components/UI/Input/index.jsx
+++ src/components/UI/Input/index.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import css from './styles.module.scss';
+import CheckIcon from '../../../images/icon/done.svg';
+import CrossIcon from '../../../images/icon/errorClose.svg';
 import EyeIcon from '../../../images/icon/eye.svg';
 
-const Input = ({ prefix, ...props }) => {
+const Input = ({ prefix, isSuccess, ...props }) => {
   const [hideValue, setHideValue] = useState(false);
   const handleClick = () => {
     setHideValue(!hideValue);
   };
 
   return (
-    <div className={css.root}>
+    <div className={`${css.root} ${isSuccess ? css.success : css.error}`}>
       {prefix && <div className={css.prefix}>{prefix()}</div>}
       <span onClick={handleClick} className={`${css.icon} ${css.eyeIcon}`}><EyeIcon/></span>
       <input className={css.input} type={`${hideValue? 'password' : 'text'}`} {...props} />
+      <span className={css.icon}>{isSuccess ? <CheckIcon/> : <CrossIcon/>}</span>
     </div>
   );
 };
 
 Input.propTypes = {
   prefix: PropTypes.func,
+  isSuccess: PropTypes.bool,
 };
 
 Input.defaultProps = {
